3b3883d17e7d094b7be4f3216e85e7d1fbac9892
byewhile <93428940+byewhile@users.noreply.github.com>
2024-09-19 13:41:19 +0300

    update find-emoji game

--- src/pages/find_emoji/Find_emoji.jsx
+++ src/pages/find_emoji/Find_emoji.jsx
@@ -5,27 +5,36 @@ import { useState, useEffect } from "react";
 const Find_emoji = () => {
   const [level, setLevel] = useState(1);
   const [correctEmoji, setCorrectEmoji] = useState("");
-  const [emojiInGame, setEmojiInGame] = useState(["😂", "🤣", "😥", "😢", "🙄", "🤤", "😭", "🥺"]);
-
-  const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  }
+  const [emojisInGame, setEmojisInGame] = useState([]);
 
   const levelUp = () => {
     setLevel(level + 1);
   }
 
+  const shuffle = (array) => {
+    array.sort(() => Math.random() - 0.5);
+  }
+
   useEffect(() => {
-    setCorrectEmoji(emojiInGame[getRandomInt(emojiInGame.length)]);
-    setEmojiInGame([...emojiInGame].sort(() => Math.random() - 0.5));
+    let prepareEmojisInGame = ["😂", "🤣", "😥", "😢", "🙄", "🤤", "😭", "🥺", "👿", "🥰", "😉", "😊", "😘", "😲", "😀", "😈", "😠"];
+    shuffle(prepareEmojisInGame);
+    const prepareCorrectEmoji = prepareEmojisInGame.shift();
+
+    for (let i = 0; i < level; i++) {
+      prepareEmojisInGame = prepareEmojisInGame.concat(prepareEmojisInGame);
+    }
+    prepareEmojisInGame[0] = prepareCorrectEmoji;
+    shuffle(prepareEmojisInGame);
+
+    setCorrectEmoji(prepareCorrectEmoji);
+    setEmojisInGame(prepareEmojisInGame);
   }, [, level])
   
   return (
     <main className="find_emojiPage">
       <h2><span>Найди: {correctEmoji}</span><span>Уровень: {level}</span></h2>
-
       <div className="gameField">
-        {emojiInGame.map((emoji, index) => (
+        {emojisInGame.map((emoji, index) => (
           <div key={index}>
           {correctEmoji == emoji ?
             <span onClick={levelUp}>{emoji}</span>
